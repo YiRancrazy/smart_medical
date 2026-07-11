49d97899e752dae726b7e23b4bb67174ce87981a
YiRanCrazy <yirancrazy@gmail.com>
2026-07-11 14:20:29 +0800

    build: 启用 MyBatis-Plus 自动填充

--- src/main/java/com/yirancrazy/smartmedical/config/MybatisPlusConfig.java
+++ src/main/java/com/yirancrazy/smartmedical/config/MybatisPlusConfig.java
@@ -1,19 +1,21 @@
-//package com.yirancrazy.smartmedical.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-///**
-// * @Author: YiRanCrazy@gmail.com
-// * @Description:
-// * @Datetime: 2026-02-02 19:11
-// * @Version: 1.0
-// */
-//
-//@Configuration
-//public class MybatisPlusConfig {
-//    @Bean
-//    public MybatisPlusMetaObjectHandler myMetaObjectHandler() {
-//        return new MybatisPlusMetaObjectHandler();
-//    }
-//}
+package com.yirancrazy.smartmedical.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis-Plus 配置
+ * @Author: YiRanCrazy@gmail.com
+ * @Description: 注册自动填充处理器（create_time/update_time/deleted）
+ * @Datetime: 2026-02-02 19:11
+ * @Version: 1.0
+ */
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusMetaObjectHandler myMetaObjectHandler() {
+        return new MybatisPlusMetaObjectHandler();
+    }
+}
