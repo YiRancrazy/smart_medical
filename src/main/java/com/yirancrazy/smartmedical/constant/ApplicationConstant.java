45e7efecd7913b340aa6c17011a82149d9e0489a
YiRanCrazy <yirancrazy@gmail.com>
2026-07-20 16:43:06 +0800

    fix(knife4j): 修复knife4j 接口不显示的问题, 更换第三方 knife4j 组件

---
+++ src/main/java/com/yirancrazy/smartmedical/constant/ApplicationConstant.java
@@ -0,0 +1,9 @@
+package com.yirancrazy.smartmedical.constant;
+
+/**
+ * @Author: YiRanCrazy@gmail.com
+ * @Description: TODO
+ * @Version: 1.0
+ * @DateTime: 2026/7/20 15:15
+ **/public class BusinessConstant {
+}
