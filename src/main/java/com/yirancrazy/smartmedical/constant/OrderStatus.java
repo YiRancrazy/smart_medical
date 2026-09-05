34c6263f137aa161bc442a2fd32d289d821180c2
YiRanCrazy <#>
2026-09-05 09:51:23 +0800

    refactor(订单): 删除未使用的退款中间状态仅保留已退款

--- src/main/java/com/yirancrazy/smartmedical/constant/OrderStatus.java
+++ src/main/java/com/yirancrazy/smartmedical/constant/OrderStatus.java
@@ -13,13 +13,7 @@ public enum OrderStatus {
     CANCELED("已取消", 2),
     FINISHED("已完成", 3),
     FAILED("支付失败", 4),
-    REFUNDING("退款中", 5),
-    REFUNDED("已退款", 6),
-    REFUND_FAILED("退款失败", 7),
-    REFUND_CANCELED("退款取消", 8),
-    REFUND_TIMEOUT("退款超时", 9),
-    REFUND_REJECTED("退款拒绝", 10),
-    REFUND_REQUESTED("退款申请", 11);
+    REFUNDED("已退款", 6);
 
     private final String description;
     private final int code;
